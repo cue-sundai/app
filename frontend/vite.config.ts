bad16ef2aa4b6a3578198d2751a8e2d2a2f24d04
Nathan Alam <nathanalam99@gmail.com>
2026-03-01 19:09:04 -0500

    Fix loading assets

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
+  base: command === 'build' ? '/app/' : '/',
   server: {
     proxy: {
       '/api': 'http://localhost:8820',
@@ -13,4 +14,4 @@ export default defineConfig({
       },
     },
   },
-})
+}))
